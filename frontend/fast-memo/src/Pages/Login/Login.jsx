c07008bce3232b3b550762ca8a0006910c8471eb
unknown <ikeanyi_joseph@yahoo.com.>
2024-05-21 15:57:21 +0100

    update on Login Page

--- frontend/fast-memo/src/Pages/Login/Login.jsx
+++ frontend/fast-memo/src/Pages/Login/Login.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import { Link } from "react-router-dom";
-import Passwordinput from "../../Components/input/Passwordinput";
+import PasswordInput from "../../Components/input/PasswordInput";
 import { validateEmail } from "../../utils/helper";
-const Login = () => {
 
+const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
@@ -16,14 +16,22 @@ const Login = () => {
       setError("Please enter a valid email address.");
       return;
     }
- };
+
+    if (!password) {
+      setError("Please enter the password.");
+      return;
+    }
+
+    setError("");
+    // Add your login logic here
+  };
 
   return (
     <>
       <Navbar />
-      <div className=" bg-white flex text-center justify-center mt-28">
+      <div className="bg-white flex text-center justify-center mt-28">
         <div className="w-96 border rounded bg-white px-7 py-10">
-          <form on onSubmit={handleLogin}>
+          <form onSubmit={handleLogin}>
             <h4 className="text-2xl mb-9 text-black">Login</h4>
             <input
               type="text"
@@ -32,9 +40,13 @@ const Login = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
-            <Passwordinput
-             value={password}
-             onChange={(e) => setPassword(e.target.value)} />
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+            
             <button
               type="submit"
               className="btn-primary w-full text-sm bg-primary text-white p-2 rounded hover:bg-blue-600"
@@ -42,12 +54,11 @@ const Login = () => {
               Login
             </button>
             <p className="text-sm text-center mt-4">
-              Not Registered?{""}
+              Not Registered?{" "}
               <Link
                 to="/signup"
-                className=" font-medium text-primary underline"
+                className="font-medium text-primary underline"
               >
-                {" "}
                 Create an Account.
               </Link>
             </p>
