7b226a00e6c9241396ed3624f31520703d23b842
unknown <ikeanyi_joseph@yahoo.com.>
2024-05-24 16:26:57 +0100

    Added update on CardNote

--- frontend/fast-memo/src/Pages/Home/Home.jsx
+++ frontend/fast-memo/src/Pages/Home/Home.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import Navbar from '../../Components/Navbar/Navbar'
-
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import NoteCard from "../../Components/Cards/NoteCard";
 
 const Home = () => {
   return (
     <div>
-     <Navbar/>
+      <Navbar />
+      <div className=" container mx-auto">
+        <NoteCard
+          title="Meeting with the team on 23rd May"
+          date="23rd May 2024"
+          content="Meeting with the team on 23rd MayMeeting with the team on 23rd May"
+          tags="#Meeting"
+          isPinned={true}
+          onEdit={()=>{}}
+          onDelete={()=>{}}
+          onPinNote={()=>{}}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
