c6177204c954bb61b998f01b4db6d05896eb7e17
Joseph <ikeanyi_joseph@yahoo.com>
2024-06-09 16:51:52 +0100

    backend update

--- frontend/fast-memo/src/Pages/Home/AddEditNotes.jsx
+++ frontend/fast-memo/src/Pages/Home/AddEditNotes.jsx
@@ -1,13 +1,48 @@
 import React, { useState } from "react";
 import TagInput from "../../Components/input/TagInput";
+import { MdClose } from "react-icons/md";
 
-const AddEditNotes = () => {
+const AddEditNotes = ({ noteData, type, onClose }) => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [tags, setTags] = useState([]);
+  const [error, setError] = useState(null);
+
+  // Add Note
+  const addNewNote = async () => {};
+
+  // Edit Note
+  const editNote = async () => {};
+
+  const handleAddNote = () => {
+    if (!title) {
+      setError("Please enter the title");
+      return;
+    }
+
+    if (!content) {
+      setError("Please enter the content");
+      return;
+    }
+
+    setError("");
+
+    if (type === "edit") {
+      editNote;
+    } else {
+      addNewNote;
+    }
+  };
 
   return (
-    <div>
+    <div className="relative">
+      <button
+        className="w-10 h-10 flex rounded-full items-center justify-center absolute -top-3 -right-3 hover:bg-blue-300"
+        onClick={onClose}
+      >
+        <MdClose className="text-xl  text-slate-400 hover:text-white" />
+      </button>
+
       <div className="flex flex-col gap-2">
         <label className="input-label">TITLE</label>
         <input
@@ -33,8 +68,12 @@ const AddEditNotes = () => {
         <label className="input-label"> TAGS</label>
         <TagInput tags={tags} setTags={setTags} />
       </div>
+      {error && <p className="text-xs text-red-500 pt-4">{error}</p>}
 
-      <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>
+      <button
+        className="btn-primary font-medium mt-5 p-3"
+        onClick={handleAddNote}
+      >
         ADD
       </button>
     </div>
--- frontend/fast-memo/src/Pages/Home/Home.jsx
+++ frontend/fast-memo/src/Pages/Home/Home.jsx
@@ -1,55 +1,61 @@
 import React, { useState } from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import NoteCard from "../../Components/Cards/NoteCard";
-import {MdAdd, MdOutlineAlarmAdd} from "react-icons/md"
+import { MdAdd, MdOutlineAlarmAdd } from "react-icons/md";
 import AddEditNotes from "./AddEditNotes";
-import Modal from "react-modal"
+import Modal from "react-modal";
 
 const Home = () => {
-  const[openAddEditModal, setOpenAddEditModal] = useState({
+  const [openAddEditModal, setOpenAddEditModal] = useState({
     isShown: false,
     type: "add",
     data: null,
-  })
+  });
 
   return (
     <div>
       <Navbar />
       <div className=" container mx-auto">
         <div className="grid grid-cols-3 gap-4 mt-8">
-        <NoteCard
-          title="Meeting with the team on 23rd May"
-          date="23rd May 2024"
-          content="Meeting with the team on 23rd MayMeeting with the team on 23rd May"
-          tags="#Meeting"
-          isPinned={true}
-          onEdit={()=>{}}
-          onDelete={()=>{}}
-          onPinNote={()=>{}}
-        />
+          <NoteCard
+            title="Meeting with the team on 23rd May"
+            date="23rd May 2024"
+            content="Meeting with the team on 23rd MayMeeting with the team on 23rd May"
+            tags="#Meeting"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
         </div>
-        
       </div>
-      <button className="flex items-center justify-center w-16 h-16 rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10 " 
-      onClick={() => {
-        setOpenAddEditModal({isShown: true, type: "add", data: null})
-      }}>
-        <MdAdd className="text-[32px] text-white"/>
+      <button
+        className="flex items-center justify-center w-16 h-16 rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10 "
+        onClick={() => {
+          setOpenAddEditModal({ isShown: true, type: "add", data: null });
+        }}
+      >
+        <MdAdd className="text-[32px] text-white" />
       </button>
       <Modal
-       isOpen={openAddEditModal.isShown}
-       onRequestClose={() => {}}
-       style={{
-        overlay: {
-          background: "rgb(0,0,0,0.2",
-        }
-       }}
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            background: "rgb(0,0,0,0.2",
+          },
+        }}
         contentLabel=""
-        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll" 
-       >
-      <AddEditNotes />
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+      >
+        <AddEditNotes
+          type={openAddEditModal.type}
+          notedata={openAddEditModal.data}
+          onClose={() => {
+            setOpenAddEditModal({ isShown: false, type: "add", data: null });
+          }}
+        />
       </Modal>
-      
     </div>
   );
 };
